0fa8a93ea5697590bbaab48a218460a7064e2c89
Witcher-MTM <phantomit228@gmail.com>
2022-11-07 13:20:27 +0200

    React
    Calc fuel

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';  
+import CalcFuel from './CalcFuel';
+function App() {
+  
+  return (
+    <div className="App">
+      <CalcFuel/>
+    </div>
+  );
+  
+}
+export default App;
---
+++ client/src/CalcFuel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+class CalcFuel extends React.Component{
+
+    CalcuteFuel(){
+        var fuel = document.getElementById('fuel');
+        var resfuel = document.getElementById('ResFuel');
+        if(fuel.innerText === '' && resfuel.innerText === ''){
+            var h2 = document.getElementById('hResult');
+            var resultinfo = 100/resfuel.value;
+            h2.innerHTML = "Сможете проехать "+resultinfo*fuel.value+"км";
+        }
+    }
+
+
+    render() {
+        return (
+            <div id='Form'>
+                    <div className="CountFuel "> 
+                        <h3>Кол-во топлива</h3>
+                    <input type={'number'} id='fuel'></input>
+                    </div>
+                    <div className="countResFuel ">
+                        <h3>Расход топила</h3>
+                        <input type={'number'} id='ResFuel'></input>
+                    </div>
+                    <div id='result'>
+                        <h2 id='hResult'></h2>
+                    </div>
+                    <button onClick={this.CalcuteFuel} type='submit'>Calculate</button>
+            </div>
+            
+        );
+    }
+}
+
+export default CalcFuel;
